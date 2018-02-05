cwhuang/asterisk

commit 1199927fc09a57b90dd2426ae209666b3a838811

cdr_mysql.c: Apply cdrzone to start and answer
Change-Id: I7de0a5adc89824a5f2b696fc22c80fc22dff36b0

addons/cdr_mysql.c
@@ -258,9 +258,7 @@ db_reconnect:
 			/* Need the type and value to determine if we want the raw value or not */
 			if (entry->staticvalue) {
 				value = ast_strdupa(entry->staticvalue);
-			} else if ((!strcmp(cdrname, "answer") ||
-				 !strcmp(cdrname, "end") ||
-				 !strcmp(cdrname, "disposition") ||
+			} else if ((!strcmp(cdrname, "disposition") ||
 				 !strcmp(cdrname, "amaflags")) &&
 				(strstr(entry->type, "int") ||
 				 strstr(entry->type, "dec") ||
@@ -270,7 +268,8 @@ db_reconnect:
 				 strstr(entry->type, "numeric") ||
 				 strstr(entry->type, "fixed"))) {
 				ast_cdr_format_var(cdr, cdrname, &value, workspace, sizeof(workspace), 1);
-			} else if (!strcmp(cdrname, "start")) {
+			} else if (!strcmp(cdrname, "start") || !strcmp(cdrname, "answer") ||
+				 !strcmp(cdrname, "end")) {
 				struct ast_tm tm;
 				char timestr[128];
 				ast_localtime(&cdr->start, &tm, ast_str_strlen(cdrzone) ? ast_str_buffer(cdrzone) : NULL);
@@ -364,7 +363,7 @@ static void free_strings(void)
 }
 
 static int my_unload_module(int reload)
-{ 
+{
 	struct column *entry;
 
 	ast_cli_unregister_multiple(cdr_mysql_status_cli, sizeof(cdr_mysql_status_cli) / sizeof(struct ast_cli_entry));

commit 4e012662f2d982f090f76904f6a714581f9ac078

cdr_mysql: fix incorrect answer and end
It's a regression introduced by commit 1199927f.

Fixes: 1199927f ("cdr_mysql.c: Apply cdrzone to start and answer")

Signed-off-by: Chih-Wei Huang <cwhuang@linux.org.tw>

addons/cdr_mysql.c
@@ -280,7 +280,15 @@ db_reconnect:
 				 !strcmp(cdrname, "end")) {
 				struct ast_tm tm;
 				char timestr[128];
-				ast_localtime(&cdr->start, &tm, ast_str_strlen(cdrzone) ? ast_str_buffer(cdrzone) : NULL);
+				const struct timeval *tv;
+				if (!strcmp(cdrname, "start")) {
+					tv = &cdr->start;
+				} else if (!strcmp(cdrname, "answer")) {
+					tv = &cdr->answer;
+				} else {
+					tv = &cdr->end;
+				}
+				ast_localtime(tv, &tm, ast_str_strlen(cdrzone) ? ast_str_buffer(cdrzone) : NULL);
 				ast_strftime(timestr, sizeof(timestr), DATE_FORMAT, &tm);
 				value = ast_strdupa(timestr);
 			} else if (!strcmp(cdrname, "calldate")) {